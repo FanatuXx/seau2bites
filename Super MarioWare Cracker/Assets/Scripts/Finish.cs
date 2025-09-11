483a481762f2144e8e01900b7c9c784fec18f095
Bernadoxite <bernadette.te.te.te@gmail.com>
2025-09-11 23:36:35 +0200

    Stage cuisine quasi fini et fond stage psy

--- Super MarioWare Cracker/Assets/Scripts/Finish.cs
+++ Super MarioWare Cracker/Assets/Scripts/Finish.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Finish : MonoBehaviour
 
 {
+    public Animator animator;
     private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.CompareTag("Finish"))
         {
             Debug.Log("BIEN OUEJ");
             GameManager.instance.NextLevel();
+           
 
         }
     }
+
+  
+    
 }
 
