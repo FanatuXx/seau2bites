8688db613c0deb2737186b033b10befae37bdca7
FanatuXx <antoine.duff@hotmail.com>
2025-09-14 13:40:22 +0200

    cnxvxcxm

--- Super MarioWare Cracker/Assets/Scripts/Finish.cs
+++ Super MarioWare Cracker/Assets/Scripts/Finish.cs
@@ -6,12 +6,13 @@ using Unity.VisualScripting;
 public class Finish : MonoBehaviour
 
 {
-    public Animator animator;
+    public Animator animator; //anim
     private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.CompareTag("Finish"))
         {
             Debug.Log("BIEN OUEJ");
+            animator.SetBool("Finish", true);
             GameManager.instance.NextLevel();
            
 
