a007662653cf7ff5e80ed30fb7b5fcd43f0ef682
Bernadoxite <bernadette.te.te.te@gmail.com>
2025-09-14 13:03:09 +0200

    morceau de jambon

--- Super MarioWare Cracker/Assets/Scripts/CharacterController.cs
+++ Super MarioWare Cracker/Assets/Scripts/CharacterController.cs
@@ -2,101 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SocialPlatforms;
 
 public class CharacterController : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     public float speed;
     public float jumpForce; 
     private float moveInput;
+    private bool isRevesed = false;
+    private bool isJumping = false;
+    public Volume chromaticab;
 
     private bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
     public LayerMask whatIsGround;
 
-    private float jumpTimeCounter;
-    public float jumpTime;
-    private bool isJumping;
-
     public Animator animator; //anim
     bool facingRight = true; //flip
-  
-    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
-  
-       
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        Vector2 jumpDir = Vector2.up;
-        if (rb.gravityScale == -1)
-        {
-            jumpDir = -jumpDir;
-            jumpForce = -jumpForce; 
-        }
-
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            isJumping = true;
-            // jumpTimeCounter = jumpTime;
-            rb.linearVelocity = jumpDir * jumpForce;
-            Debug.Log("jumping");
-            animator.SetBool("IsJumping", true);
-
-        }
-
-
-
-
-        //    if (jumpTimeCounter > 0)
-        //    {
-        //        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        //        jumpTimeCounter -= Time.deltaTime;
-        //        animator.SetBool("IsJumping", true); //anim
-
-        //    }
-        //    else
-        //    {
-        //        isJumping = false;
-        //        animator.SetBool("IsJumping", false); //anim
-
-
-        //    }
-
-        //}
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            isJumping = false;
-            animator.SetBool("IsJumping", false);//anim
-            Debug.Log("not jumping");
-
-        }
-
-
-
-        animator.SetFloat("Speed", Mathf.Abs(moveInput)); //anim
-
-
+        
     }
 
    
     void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        float velocity;
+
+        if (this.isRevesed)
+        {
+            velocity = moveInput * speed * -1;
+        }
+        else
+        {
+            velocity = moveInput * speed;
+        }
+
+        rb.linearVelocity = new Vector2(velocity, rb.linearVelocity.y);
 
         if (moveInput > 0 && !facingRight) //flip
 
@@ -108,9 +65,40 @@ public class CharacterController : MonoBehaviour
         {
              Flip();
         }
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        Vector2 jumpDir = Vector2.up;
+        if (rb.gravityScale <= 0)
+        {
+            jumpDir = Vector2.down;
+        }
 
-       
+        if (!isGrounded)
+        {
+            this.isJumping = true;
+        }
+
+        if (this.isJumping)
+        {
+            if (isGrounded)
+            {
+                animator.SetBool("IsJumping", false);
+                this.isJumping = false;
+            }
+            
+        }
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = jumpDir * jumpForce;
+            animator.SetBool("IsJumping", true);
+        }
+
+
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput)); //anim
     }
+
+
      private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Reversegravity"))
@@ -119,7 +107,13 @@ public class CharacterController : MonoBehaviour
             Vector3 currentScale = gameObject.transform.localScale;
             currentScale.y *= -1;
             gameObject.transform.localScale = currentScale;
-
+            chromaticab.enabled = true;
+            //chromaticab.active = true;
+        }
+        if (other.gameObject.CompareTag("Reversecommands"))
+        {
+            this.isRevesed = !this.isRevesed;
+            this.facingRight = !this.facingRight;   
         } 
     }
 
