7129fef9faed3b9adc6d992795b721b540835030
FanatuXx <antoine.duff@hotmail.com>
2025-10-07 11:21:26 +0200

    New Input system v410 (still not working mdr)

--- Super MarioWare Cracker/Assets/Scripts/NewPlayerController.cs
+++ Super MarioWare Cracker/Assets/Scripts/NewPlayerController.cs
@@ -24,6 +24,8 @@ public class NewPlayerController : MonoBehaviour
     public Animator animator; //anim
     bool facingRight = true; //flip
 
+    bool jumpNow = false;
+
 
     private void Awake()
     {
@@ -54,12 +56,34 @@ public class NewPlayerController : MonoBehaviour
     private void DoJump(InputAction.CallbackContext context)
     {
         Debug.Log("Jump!");
+        jumpNow = true;
+    }
+
+    void Flip() //flip
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 
 
     private void Update()
     {
-      /*  if (!isGrounded)
+        Vector2 jumpDir = Vector2.up;
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (move.ReadValue<Vector2>().x > 0 && !facingRight) //flip
+        {
+            Flip();
+        }
+        if (move.ReadValue<Vector2>().x < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (!isGrounded)
         {
             this.isJumping = true;
         }
@@ -68,20 +92,75 @@ public class NewPlayerController : MonoBehaviour
         {
             if (isGrounded)
             {
-
                 this.isJumping = false;
                 animator.SetBool("IsJumping", false);
             }
         }
-      */
 
-        
+        if (rb.gravityScale <= 0)
+        {
+            jumpDir = Vector2.down;
+        }
+        /*if (isGrounded == true && jump.pressed);
+        {
+
+            rb.linearVelocity = jumpDir * jumpForce;
+            //isJumping = true;
+            animator.SetBool("IsJumping", true);
+
+        }
+        */
+
+        animator.SetFloat("Speed", Mathf.Abs(move.ReadValue<Vector2>().x)); //anim
     }
+    /*  if (!isGrounded)
+      {
+          this.isJumping = true;
+      }
+
+      if (this.isJumping)
+      {
+          if (isGrounded)
+          {
+
+              this.isJumping = false;
+              animator.SetBool("IsJumping", false);
+          }
+      }
+    */
     private void FixedUpdate()
     {
-        Debug.Log("Movement values " + move.ReadValue<Vector2>());
-        rb.linearVelocity = new Vector2(move.ReadValue<Vector2>().x * speed, rb.linearVelocity.y); //Movements latéraux
+        if (jumpNow)
+        {
+            rb.linearVelocity = new Vector2(move.ReadValue<Vector2>().x * speed, jumpForce);
+            jumpNow = false;
+        }
 
-        rb.linearVelocity = new Vector2(jump.action.ReadValue<float>() * jumpForce, rb.linearVelocity.y); //Jump
+        else
+        {
+            rb.linearVelocity = new Vector2(move.ReadValue<Vector2>().x * speed, rb.linearVelocity.y);
+        }
+
+        Debug.Log("Movement values " + move.ReadValue<Vector2>());
+        //rb.linearVelocity = new Vector2(move.ReadValue<Vector2>().x * speed, jump.action.ReadValue<float>() * jumpForce); //Movements latéraux
+
+        //rb.linearVelocity = new Vector2(jump.action.ReadValue<float>() * jumpForce, rb.linearVelocity.y); //Jump
+        //Debug.Log(jump.action);
+
+        /*float velocity;
+
+
+        if (this.isRevesed)
+        {
+            velocity = move.ReadValue<Vector2>().x * speed * -1;
+            //this.facingRight = !this.facingRight;
+        }
+        else
+        {
+            velocity = move.ReadValue<Vector2>().x * speed;
+        }
+
+        rb.linearVelocity = new Vector2(velocity, rb.linearVelocity.y);
+        */
     }
 }
