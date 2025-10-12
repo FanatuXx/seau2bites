bad22d97440a438f1892a0e8fb2501f2dd249222
Bernadoxite <bernadette.te.te.te@gmail.com>
2025-10-12 15:27:05 +0200

    Mi homme mi chèvre

--- Super MarioWare Cracker/Assets/Scripts/cammovement.cs
+++ Super MarioWare Cracker/Assets/Scripts/cammovement.cs
@@ -4,8 +4,8 @@ public class cammovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Example using MoveTowards in Unity
-    Vector3 startPosition = new Vector3(1.8f, 0, 0);
-    Vector3 targetPosition = new Vector3(1.8f, -280f, 0);
+    Vector3 startPosition = new Vector3(1.8f, 7.6f, 0);
+    Vector3 targetPosition = new Vector3(1.8f, -275f, 0);
     float moveSpeed = 1f;
 
     void Update()
