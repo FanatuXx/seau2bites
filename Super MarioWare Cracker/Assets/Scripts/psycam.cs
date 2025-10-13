87e910619c260a477c34725f7d9c37aad4f80a80
Bernadoxite <bernadette.te.te.te@gmail.com>
2025-10-13 12:22:22 +0200

    8 gigas dans le stage aqua

--- Super MarioWare Cracker/Assets/Scripts/psycam.cs
+++ Super MarioWare Cracker/Assets/Scripts/psycam.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class cammovement : MonoBehaviour
+public class psycam : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Example using MoveTowards in Unity
     Vector3 startPosition = new Vector3(1.8f, 2.7f, 0);
-    Vector3 targetPosition = new Vector3(1.8f, -275f, 0);
+    Vector3 targetPosition = new Vector3(1.8f, -145f, 0);
     float moveSpeed = 1f;
 
     void Update()
