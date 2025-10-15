cd14dd9656fd3849a085ddeb97cf0af77ceab484
Bernadoxite <bernadette.te.te.te@gmail.com>
2025-10-15 23:14:42 +0200

    jumpboost

--- Super MarioWare Cracker/Assets/Scripts/Jumpboost.cs
+++ Super MarioWare Cracker/Assets/Scripts/Jumpboost.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SocialPlatforms;
 
 public class Jumpboost : MonoBehaviour
 {
     public float jumpboost = 1.1f;
+    public Volume postex;
     //public float m_duration = 5f;
     //private float tempjump;
-    
 
 
-    //private void Awake()
-    //{
-    //    jumpboost = gameObject.GetComponent<CharacterController>().jumpForce;
-    //}
+
+    private void Start()
+    {
+        postex.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,7 +32,20 @@ public class Jumpboost : MonoBehaviour
 
             gameObject.GetComponent<CharacterController>().jumpForce *= jumpboost;
             other.gameObject.SetActive(false);
+            postex.enabled = true;
+            StartCoroutine(ResetJump(other));
         }
+
+        IEnumerator ResetJump(Collider2D other)
+        {
+            yield return new WaitForSeconds(15);
+            gameObject.GetComponent<CharacterController>().jumpForce = 8;
+            
+            postex.enabled = false;
+            other.gameObject.SetActive(true);
+        }
+
+        
     }
 
 }
